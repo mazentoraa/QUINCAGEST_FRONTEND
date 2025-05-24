73db466002912d10856d03a214477fd74e63389e
Akram <akrambenghanem76@gmail.com>
2025-05-24 07:09:50 +0100

    feat: Add loading spinner to PrivateRoute for improved user experience during authentication

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, { useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from 'antd';
-import SideMenu from './components/SideMenu';
-import Dashboard from './components/Dashboard';
-import StockManagement from './features/stock/components/StockManagement';
-import ClientMaterialManagement from './features/clientMaterials/components/ClientMaterialManagement';
-import ClientManagementPage from './features/clientManagement';
-import ClientRawMaterialsPage from './features/clientManagement/components/ClientRawMaterialsPage';
-import OrderManagement from './components/Orders/OrderManagement';
-import InstallmentManagement from './features/installments/components/InstallmentManagement';
-import Navigation from './components/Navigation';
-import { StockProvider } from './features/stock/contexts/StockContext';
-import { ClientMaterialProvider } from './features/clientMaterials/contexts/ClientMaterialContext';
-import { OrderProvider } from './contexts/OrderContext';
-import { InstallmentProvider } from './features/installments/contexts/InstallmentContext';
-import { AuthProvider, AuthContext } from './features/authentication/context/AuthContext';
-import LoginForm from './features/authentication/components/LoginForm';
-import AuthService from './services/AuthService';
-import './App.css';
-import { ManifestePage, WorkManagementPage } from './features/manifeste/components';
-import { ProductProvider } from './features/products/contexts/ProductContext';
-import BonLivraisonReception from './components/BonsDevis/BonLivraisonReception';
-import BonLivraisonDecoupe from './components/BonsDevis/BonLivraisonDecoupe';
-import BonRetour from './components/BonsDevis/BonRetour';
-import BonCommande from './components/BonsDevis/BonCommande';
-import Devis from './components/BonsDevis/Devis';
+import React, { useEffect, useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Layout } from "antd";
+import SideMenu from "./components/SideMenu";
+import Dashboard from "./components/Dashboard";
+import StockManagement from "./features/stock/components/StockManagement";
+import ClientMaterialManagement from "./features/clientMaterials/components/ClientMaterialManagement";
+import ClientManagementPage from "./features/clientManagement";
+import ClientRawMaterialsPage from "./features/clientManagement/components/ClientRawMaterialsPage";
+import OrderManagement from "./components/Orders/OrderManagement";
+import InstallmentManagement from "./features/installments/components/InstallmentManagement";
+import Navigation from "./components/Navigation";
+import { StockProvider } from "./features/stock/contexts/StockContext";
+import { ClientMaterialProvider } from "./features/clientMaterials/contexts/ClientMaterialContext";
+import { OrderProvider } from "./contexts/OrderContext";
+import { InstallmentProvider } from "./features/installments/contexts/InstallmentContext";
+import {
+  AuthProvider,
+  AuthContext,
+} from "./features/authentication/context/AuthContext";
+import LoginForm from "./features/authentication/components/LoginForm";
+import AuthService from "./services/AuthService";
+import "./App.css";
+import {
+  ManifestePage,
+  WorkManagementPage,
+} from "./features/manifeste/components";
+import { ProductProvider } from "./features/products/contexts/ProductContext";
+import BonLivraisonReception from "./components/BonsDevis/BonLivraisonReception";
+import BonLivraisonDecoupe from "./components/BonsDevis/BonLivraisonDecoupe";
+import BonRetour from "./components/BonsDevis/BonRetour";
+import BonCommande from "./components/BonsDevis/BonCommande";
+import Devis from "./components/BonsDevis/Devis";
 
+import { Spin } from "antd"; // Import Spin
 
 const { Header, Content, Sider } = Layout;
 
@@ -34,7 +46,18 @@ const PrivateRoute = ({ children }) => {
 
   if (loading) {
     // You can render a loading spinner or a placeholder here
-    return <div>Loading...</div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Spin size="large" />
+      </div>
+    );
   }
 
   return isAuthenticated ? children : <Navigate to="/login" />;
@@ -51,59 +74,127 @@ function App() {
       <AuthProvider>
         <Routes>
           {/* Authentication routes */}
-          <Route path="/login" element={<LoginForm onSuccess={() => window.location.href = "/"} />} />
-          
-          {/* Protected routes */}
-          <Route path="/*" element={
-            <PrivateRoute>
-              <StockProvider>
-                <ClientMaterialProvider>
-                  <OrderProvider>
-                    <InstallmentProvider>
-                      <ProductProvider>
-                        <Layout style={{ minHeight: '100vh' }}>
-                          <Header className="app-header">
-                            <Navigation />
-                          </Header>
-                          <Layout>
-                            <Sider theme="light" width={300} className="app-sider">
-                              <SideMenu />
-                            </Sider>
-                            <Content className="main-content">
-                              <Routes>
-                                <Route path="/" element={<Dashboard />} />
-                                <Route path="/stock" element={<StockManagement />} />
-                                <Route path="/stock/produits" element={<StockManagement />} />
-                                <Route path="/stock/matieres" element={<ClientMaterialManagement />} />
-                                <Route path="/client-materials" element={<ClientMaterialManagement />} />
-                                <Route path="/client-materials/:client_id" element={<ClientRawMaterialsPage />} />
-                                <Route path="/orders" element={<OrderManagement />} />
-                                <Route path="/manifeste/travaux" element={<WorkManagementPage />} />
-                                <Route path="/manifeste" element={<ManifestePage />} />
-                                <Route path="/manifeste/bons" element={<ManifestePage />} />
-                                {/* <Route path="/manifeste/inventaire" element={<ManifestePage />} /> */}
-                                <Route path="/reglements/factures" element={<OrderManagement />} />
-                                <Route path="/reglements/traites" element={<InstallmentManagement />} />
-                                <Route path="/reglements/rapport" element={<Dashboard />} />
-                                <Route path="/installments" element={<InstallmentManagement />} />
-                                <Route path="/clients" element={<ClientManagementPage />} />
-                                <Route path="/bons/livraison-reception" element={<BonLivraisonReception />} />
-                                <Route path="/bons/livraison-decoupe" element={<BonLivraisonDecoupe />} />
-                                <Route path="/bons/retour" element={<BonRetour />} />
-                                <Route path="/bons/commande" element={<BonCommande />} />
-                                <Route path="/bons/devis" element={<Devis />} />
+          <Route
+            path="/login"
+            element={
+              <LoginForm onSuccess={() => (window.location.href = "/")} />
+            }
+          />
 
-                              </Routes>
-                            </Content>
+          {/* Protected routes */}
+          <Route
+            path="/*"
+            element={
+              <PrivateRoute>
+                <StockProvider>
+                  <ClientMaterialProvider>
+                    <OrderProvider>
+                      <InstallmentProvider>
+                        <ProductProvider>
+                          <Layout style={{ minHeight: "100vh" }}>
+                            <Header className="app-header">
+                              <Navigation />
+                            </Header>
+                            <Layout>
+                              <Sider
+                                theme="light"
+                                width={300}
+                                className="app-sider"
+                              >
+                                <SideMenu />
+                              </Sider>
+                              <Content className="main-content">
+                                <Routes>
+                                  <Route path="/" element={<Dashboard />} />
+                                  <Route
+                                    path="/stock"
+                                    element={<StockManagement />}
+                                  />
+                                  <Route
+                                    path="/stock/produits"
+                                    element={<StockManagement />}
+                                  />
+                                  <Route
+                                    path="/stock/matieres"
+                                    element={<ClientMaterialManagement />}
+                                  />
+                                  <Route
+                                    path="/client-materials"
+                                    element={<ClientMaterialManagement />}
+                                  />
+                                  <Route
+                                    path="/client-materials/:client_id"
+                                    element={<ClientRawMaterialsPage />}
+                                  />
+                                  <Route
+                                    path="/orders"
+                                    element={<OrderManagement />}
+                                  />
+                                  <Route
+                                    path="/manifeste/travaux"
+                                    element={<WorkManagementPage />}
+                                  />
+                                  <Route
+                                    path="/manifeste"
+                                    element={<ManifestePage />}
+                                  />
+                                  <Route
+                                    path="/manifeste/bons"
+                                    element={<ManifestePage />}
+                                  />
+                                  {/* <Route path="/manifeste/inventaire" element={<ManifestePage />} /> */}
+                                  <Route
+                                    path="/reglements/factures"
+                                    element={<OrderManagement />}
+                                  />
+                                  <Route
+                                    path="/reglements/traites"
+                                    element={<InstallmentManagement />}
+                                  />
+                                  <Route
+                                    path="/reglements/rapport"
+                                    element={<Dashboard />}
+                                  />
+                                  <Route
+                                    path="/installments"
+                                    element={<InstallmentManagement />}
+                                  />
+                                  <Route
+                                    path="/clients"
+                                    element={<ClientManagementPage />}
+                                  />
+                                  <Route
+                                    path="/bons/livraison-reception"
+                                    element={<BonLivraisonReception />}
+                                  />
+                                  <Route
+                                    path="/bons/livraison-decoupe"
+                                    element={<BonLivraisonDecoupe />}
+                                  />
+                                  <Route
+                                    path="/bons/retour"
+                                    element={<BonRetour />}
+                                  />
+                                  <Route
+                                    path="/bons/commande"
+                                    element={<BonCommande />}
+                                  />
+                                  <Route
+                                    path="/bons/devis"
+                                    element={<Devis />}
+                                  />
+                                </Routes>
+                              </Content>
+                            </Layout>
                           </Layout>
-                        </Layout>
-                      </ProductProvider>
-                    </InstallmentProvider>
-                  </OrderProvider>
-                </ClientMaterialProvider>
-              </StockProvider>
-            </PrivateRoute>
-          } />
+                        </ProductProvider>
+                      </InstallmentProvider>
+                    </OrderProvider>
+                  </ClientMaterialProvider>
+                </StockProvider>
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </Router>
