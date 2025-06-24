c909db5a064a46caf6c1e281240773f77969fd9c
bachri fatima ezzahra <109394388+Faty131@users.noreply.github.com>
2025-06-24 10:38:18 +0100

    traite

--- src/features/installments/services/InstallmentService.js
+++ src/features/installments/services/InstallmentService.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import { getApiService } from '../../../services/apiServiceFactory';
 
+// Détecte automatiquement l'URL d'API selon l'environnement
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api";
+
+
+
 const { cdsService } = getApiService();
 
 export const getPlansTraite = async () => {
   const data = await cdsService.getPlansTraite();
-  console.log("!!traiite",data);
+  console.log("!!traiite", data);
   return data;
 };
 
@@ -15,13 +21,16 @@ export const createPlanTraite = async (planData) => {
 };
 
 export const updateTraiteStatus = async (traiteId, statusData) => {
-  const response = await axios.patch(`http://localhost:8000/api/traites/${traiteId}/update-status/`, statusData);
+  const response = await axios.patch(
+    `${API_BASE_URL}/api/traites/${traiteId}/update-status/`,
+    statusData
+  );
   return response.data;
 };
 
 export const updatePlanStatus = async (planId, statusData) => {
   const response = await axios.put(
-    `http://localhost:8000/api/plans-traite/${planId}/update-status/`,
+    `${API_BASE_URL}/api/plans-traite/${planId}/update-status/`,
     statusData
   );
   return response.data;
